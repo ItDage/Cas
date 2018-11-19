08046e2b4e2a7bd3ef83f69afa73d19c5d682f74
sunchuiyou <sunchuiyou@thunisoft.com>
2018-11-19 18:32:27 +0800

    提交

--- src/main/java/com/itdage/CasApplication.java
+++ src/main/java/com/itdage/CasApplication.java
@@ -1,8 +1,8 @@
 package com.itdage;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CasApplication {
